cef48ca6e24c9d1c4be02990c3fa0cb3fade2df0
brendlij <brendlij@dhbw-loerrach.de>
2025-03-30 17:31:59 +0200

    console clear at start and --no-deprecation flag

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 // main.js
 import { mainMenu } from "./cli/menu.js";
 
+// Clear the console right at program start
+console.clear();
+
+// Start the main menu
+console.log("Welcome to the Task Manager!");
 mainMenu();
